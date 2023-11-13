6931d360ae280084b8699ca67a4cd199c42cdc88
Marcelo-MDQ <mmenendez@hotmail.com>
2023-11-13 20:44:11 -0300

    Bd mySQl corteyconfeccion

--- corteyconfeccion/settings.py
+++ corteyconfeccion/settings.py
@@ -74,13 +74,26 @@ WSGI_APPLICATION = 'corteyconfeccion.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
+DATABASES = {  
+  'default': {  
+     'ENGINE': 'django.db.backends.mysql',  
+     'NAME': 'corteyconfeccion',  
+    'USER': 'root',
+     'PASSWORD': '',
+     'HOST': 'localhost',
+     'PORT': 3306,
+     'OPTIONS': {
+       'sql_mode': 'traditional',
+        }
+   }  
+ }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
